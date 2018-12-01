3f68bf0c68614bab2a76aa771e3ecab1313d297f
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-12-02 01:18:19 +0900

    관리자 페이지 생성 및 admin 생성

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User,User_Study,User_Study_Assignment,Study,Assignment,Board,attendance,meeting
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(User_Study)
+admin.site.register(User_Study_Assignment)
+admin.site.register(Study)
+admin.site.register(Assignment)
+admin.site.register(Board)
+admin.site.register(attendance)
+admin.site.register(meeting)
+
