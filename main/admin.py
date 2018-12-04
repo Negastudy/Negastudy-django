208e9c97259823ac2c534db7bc96bec3473a3b76
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-12-04 16:39:20 +0900

    admin 모델 클래스명 수정

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User_Study,User_Study_Assignment,Study,Assignment,Board,attendance,meeting
+from .models import User_Study,User_Study_Assignment,Study,Assignment,Board,Attendance,Meeting
 from django.contrib.auth.models import User
 
 
@@ -8,6 +8,6 @@ admin.site.register(User_Study_Assignment)
 admin.site.register(Study)
 admin.site.register(Assignment)
 admin.site.register(Board)
-admin.site.register(attendance)
-admin.site.register(meeting)
+admin.site.register(Attendance)
+admin.site.register(Meeting)
 
