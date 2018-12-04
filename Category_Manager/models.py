281f9ff0dd95752a7049216280cd0707f33f5373
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-12-04 18:36:21 +0900

    카테고리 및 컴퍼니 클래스 id명 바꿈

--- Category_Manager/models.py
+++ Category_Manager/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
-	id = models.IntegerField()
+	_id = models.IntegerField()
 	name = models.CharField(max_length=200)
 
 
 class Company(models.Model):
-	id = models.IntegerField()
+	_id = models.IntegerField()
 	name = models.CharField(max_length=200)
 
