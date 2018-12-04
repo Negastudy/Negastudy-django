f8e17c3ddc7e7561b6ab73f97708a4086d9947a6
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-12-04 16:36:10 +0900

    Revert "gitignore 수정 및 admin 모델 클래스 명 수정"
    This reverts commit e8513c28429fa83106ca8875cdcd34a017424bb1.

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User_Study,User_Study_Assignment,Study,Assignment,Board,Attendance,Meeting
+from .models import User_Study,User_Study_Assignment,Study,Assignment,Board,attendance,meeting
 from django.contrib.auth.models import User
 
 
@@ -8,6 +8,6 @@ admin.site.register(User_Study_Assignment)
 admin.site.register(Study)
 admin.site.register(Assignment)
 admin.site.register(Board)
-admin.site.register(Attendance)
-admin.site.register(Meeting)
+admin.site.register(attendance)
+admin.site.register(meeting)
 
