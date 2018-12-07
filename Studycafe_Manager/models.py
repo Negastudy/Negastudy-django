f222f4b22ce483c569cbeca7fd0f01cfbb931d1c
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-12-07 14:27:59 +0900

    모델수정

--- Studycafe_Manager/models.py
+++ Studycafe_Manager/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Cafe(models.Model):
 	name = models.CharField(max_length=200)
-	location = models.TextField()
+	location = models.TextField(default=True)
 	# 추가 정보 필요
 
