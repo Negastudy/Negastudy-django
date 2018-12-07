05681f8e0078cfc15ee1241d17c6da0b0f4d79d1
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-12-07 14:27:27 +0900

    모델수정

--- main/models.py
+++ main/models.py
@@ -1,17 +1,39 @@
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import date
+
 
 # CharField는 임시적으로 모두 200글자 제한으로 둠
 
-
 class Study(models.Model):
-	category = models.IntegerField(null=True)
 	name = models.CharField(max_length=200)
+	category = models.IntegerField(null=True)
 	company = models.IntegerField(null=True)
+	# limit = models.IntegerField(null=True)
+	complete = models.BooleanField(default=False)
+	startTime = models.DateField(blank=True, null=True)
+	endTime = models.DateField(blank=True, null=True)
 	id = models.AutoField(primary_key=True)
-	limit = models.IntegerField(null=True)
-	complete = models.IntegerField(null=True)
+	members = models.ManyToManyField(
+		settings.AUTH_USER_MODEL,
+		through='User_Study',
+		through_fields=('study', 'user'),
+	)
+
+	def save(self, *args, **kwargs):
+		super(Study, self).save(*args, **kwargs)
+		if self.endTime < date.today():
+			self.complete = True
+		super(Study, self).save()
+
+
+
+	def add_members():
+		''' 스터디 멤버 추가 '''
+		pass
 
 	def __str__(self):
 		return self.name
@@ -39,7 +61,7 @@ class Board(models.Model):
 
 
 class User_Study(models.Model):
-	user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True)
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
 	study = models.ForeignKey(Study, on_delete=models.CASCADE, null=True)
 
 
@@ -51,7 +73,7 @@ class Meeting(models.Model):
 class Attendance(models.Model):
 	user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True)
 	meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE, null=True)
-	location = models.TextField()
+	location = models.TextField(default=True)
 
 
 class User_Study_Assignment(models.Model):
