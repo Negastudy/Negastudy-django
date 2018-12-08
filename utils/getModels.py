a4c57b6b217ed6b77ce815f5ca16a8f729e07d67
DESKTOP-8BQHLS5\stonehye <stonehye@sogang.ac.kr>
2018-12-08 20:00:57 +0900

    쿼리셋을 이용하여 필요한 정보 꺼내는 함수 추가

---
+++ utils/getModels.py
@@ -0,0 +1,30 @@
+from Category_Manager.models import Category, Company
+from Studycafe_Manager.models import Cafe
+from main.models import User_Study,User_Study_Assignment,Study,Assignment,Board,Attendance,Meeting
+
+
+def getCategory(n):
+	cat = Category.objects.get(_id=n)
+	return cat.name
+
+
+def getNotice(pk):
+	try:
+		# boards = Board.objects.get(study=pk)
+		title = Board.objects.filter(study=pk).latest('title')
+	except Board.DoesNotExist:
+		title = None
+
+	return " "
+
+
+def getPeoplenames(pk):
+	try:
+		user_study = User_Study.objects.filter(study=pk)
+		people = []
+		for element in user_study:
+			people.append(element.user)
+		return  people
+	except User_Study.DoesNotExist:
+		return None
+
